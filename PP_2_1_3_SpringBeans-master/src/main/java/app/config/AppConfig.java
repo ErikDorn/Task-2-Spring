87d8e20c06da4596a181649fa0d1fba66d61cb65
Вадим Дмитриенко <91070573+ErikDorn@users.noreply.github.com>
2022-06-20 18:57:26 +0300

    Task 2 Spring

---
+++ PP_2_1_3_SpringBeans-master/src/main/java/app/config/AppConfig.java
@@ -0,0 +1,19 @@
+package app.config;
+
+import app.model.AnimalsCage;
+import app.model.Dog;
+import app.model.Timer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import java.sql.Time;
+
+@Configuration
+@ComponentScan(basePackages = "app")
+public class AppConfig {
+
+}
